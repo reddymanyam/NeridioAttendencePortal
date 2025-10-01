67f78a668dde2eba3b08660e0e1862aa47a4baca
reddymanyam <reddappareddy18@gmail.com>
2025-10-01 11:31:28 +0530

    upd: app and main are updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import './index.css';
-import Navbar from './components/Navbar';
-import Authentication from './components/Authentication';
-import AdminDashboard from './AdminDashboard/AdminDashboard';
-import EmployeeDashboard from './EmployeDashboard/EmployeDashboard';
-import SignUp from './components/SignUp';
-import MarkAttendance from './components/MarkAttendence';
+import AdminLayout from './AdminLayout/AdminLayout';
+
 const App = () => {
   return (
-    // <Authentication />
-    // <Navbar />
-    // <AdminDashboard />
-    // <EmployeeDashboard />
-    <MarkAttendance />
-    // <SignUp />
+    <>
+      <AdminLayout />
+      
+    </>
+
   )
 }
 
